ba82a13634eb233e7df23b8179682245af6632d1
Alex Nechesnyuk <a.nechesnyuk@infinet.ru>
2025-02-09 00:22:19 +0300

    Vers 6

--- src/main/java/ru/otus/java/basic/FirstApp123.java
+++ src/main/java/ru/otus/java/basic/FirstApp123.java
@@ -4,8 +4,9 @@ public class FirstApp123 {
     public static void main(String[] args) {
         System.out.println("Hellow wor  ld!!!");
         System.out.println("Hellow worwwwld!!!");
+        System.out.println("Hellow worwwwld!!!");
         System.out.println("Hellow 12 world!!!");
-        System.out.println("Hellow world!!!");
+        System.out.println("Helleeeow world!!!");
         System.out.println("Hellow world!!!");
         System.out.println("Hellow world!!!");
     }
